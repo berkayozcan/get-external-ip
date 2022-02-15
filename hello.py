9206ca17913b5e4d2087a6e9e0cd2dcce41b2a6d
berkayozcan <berkay@berkayozcan.com.tr>
2022-02-15 10:40:13 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+#You can specify a Python version for pip to use:
+#pip3.4 install requests
+#Python 3.4 has pip support built-in, so you can also use:
+#python3.4 -m pip install
+#If you're running Ubuntu (or probably Debian as well), you'll need to install the system pip3 separately:
+#sudo apt-get install python3-pip
+#This will install the pip3 executable, so you can use it, as well as the earlier mentioned python3.4 -m pip:
+#pip3 install requests
+
+import time
+from requests import get
+
+import datetime;
+  
+# ct stores current time
+ct = datetime.datetime.now()
+
+
+while True:
+    ip = get('https://api.ipify.org').content.decode('utf8')
+    print('WAN IP address is: {} - Time '.format(ip))
+    print("current time:-", ct)
+    time.sleep(5)
